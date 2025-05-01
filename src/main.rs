61ed7f014dc222632d652def6a3f03a3b99a16d3
mageOfstructs <jason.puschnig@gmx.at>
2025-05-01 09:38:35 +0200

    expanded demo to showcase recursive keyword search

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(let_chains)]
+
 use std::io::{Read, Write};
 
 use console::Term;
@@ -7,13 +9,19 @@ fn main() {
     let root = TreeNode::new_keyword("BEGIN".to_string(), String::new());
     let child =
         TreeNode::new_keyword_with_parent("unsigned".to_string(), "u".to_string(), root.clone());
-    let child =
-        TreeNode::new_keyword_with_parent("int".to_string(), "i".to_string(), child.clone());
+    let child2 =
+        TreeNode::new_keyword_with_parent("signed".to_string(), "s".to_string(), root.clone());
+    let types = TreeNode::new(NodeType::Null, &child);
+
+    let int = TreeNode::new_keyword_with_parent("int".to_string(), "i".to_string(), types.clone());
+    child.borrow_mut().add_child(&types);
+    child2.borrow_mut().add_child(&types);
+
     let child = TreeNode::new(
         NodeType::UserDefined {
             final_chars: vec!['='],
         },
-        &child,
+        &int,
     );
     let child = TreeNode::new(NodeType::Null, &child);
 
@@ -23,10 +31,17 @@ fn main() {
     let terminal = Term::stdout();
     while !cursor.is_done() {
         let input = terminal.read_char().unwrap();
-        println!("{input}");
-        if let Some(res) = cursor.advance(input) {
-            print!("{} ", res);
+        match input {
+            '\x08' => cursor.clear_inputbuf(),
+            _ => {
+                if let Some(res) = cursor.advance(input) {
+                    print!("{} ", res);
+                }
+            }
         }
+        // if cursor.is_in_userdefined_stage() {
+        //     print!("{input}");
+        // }
         std::io::stdout().flush();
     }
 }
