9cd42d19db19eb0dcbdb811da551c4c13e0dcf4d
mageOfStructs <jason.puschnig@gmx.at>
2025-05-23 20:34:09 +0200

    add label for final fsm

--- src/main.rs
+++ src/main.rs
@@ -20,13 +20,14 @@ fn main() {
     ";
     // Repeat nodes:
     // identifier ::= letter { letter | digit | "_" };
-    let mut complete_ebnf = String::new();
-    File::open(&Path::new("sql.ebnf"))
-        .unwrap()
-        .read_to_string(&mut complete_ebnf)
-        .unwrap();
-    print_parsed_ebnf(&complete_ebnf);
-    if let Ok(root) = frontend::create_graph_from_ebnf(&complete_ebnf) {
+    // let mut complete_ebnf = String::new();
+    // File::open(&Path::new("sql.ebnf"))
+    //     .unwrap()
+    //     .read_to_string(&mut complete_ebnf)
+    //     .unwrap();
+    // print_parsed_ebnf(&complete_ebnf);
+    if let Ok(root) = frontend::create_graph_from_ebnf(&sql) {
+        println!("FSM:");
         root.borrow().dbg();
         let mut cursor = TreeCursor::new(&root);
 
