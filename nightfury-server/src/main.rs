43f85d39e9633e3f5d7e808cfca13c2cbe712fc3
mageOfStructs <jason.puschnig@gmx.at>
2025-06-22 00:34:54 +0200

    wrote a little bit of a structure

--- nightfury-server/src/main.rs
+++ nightfury-server/src/main.rs
@@ -1,34 +1,58 @@
+#![feature(if_let_guard)]
+use std::collections::HashMap;
 use std::io::Read;
 use std::os::unix::net::{UnixListener, UnixStream};
+use std::sync::Arc;
+use std::sync::RwLock;
 use std::thread;
 
 use lib::protocol::Request;
 use lib::{FSMCursor, FSMNode};
 
-fn handle_client(mut stream: UnixStream, cursor: FSMCursor) {
-    let mut buf = String::new(); // bad
-    stream.read_to_string(&mut buf).expect("buf read");
-    let req: Request = serde_json::from_str(&buf).expect("Request deserialize");
+fn handle_client(req: Request, cursor: &mut FSMCursor) {
     match req {
-        Request::Init(lang) => {}
         Request::Reset => {}
-        Request::GetCapabilities => {}
         Request::Advance(c) => {}
         Request::AdvanceStr(str) => {}
+        _ => unreachable!(),
     }
 }
 
 fn main() -> std::io::Result<()> {
     let listener = UnixListener::bind("./nightfury.sock")?;
-    let root = FSMNode::new_null(None);
+    let mut handles = Vec::new();
+    let fsms = Arc::new(RwLock::new(HashMap::new()));
+    fsms.write()
+        .unwrap()
+        .insert("test", FSMNode::new_null(None));
 
     // accept connections and process them, spawning a new thread for each one
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {
+            Ok(mut stream) => {
                 /* connection succeeded */
-                let cursor = FSMCursor::new(&root);
-                thread::spawn(|| handle_client(stream, cursor));
+                let fsms_clone = Arc::clone(&fsms);
+                handles.push(thread::spawn(move || {
+                    let mut buf = String::new(); // bad
+                    let mut cursor = None;
+                    while stream.read_to_string(&mut buf).expect("buf read") != 0 {
+                        let req: Request = serde_json::from_str(&buf).expect("deserde");
+                        match req {
+                            Request::Init(ref name)
+                                if let Some(fsm) =
+                                    fsms_clone.read().unwrap().get(&name.as_str()) =>
+                            {
+                                cursor = Some(FSMCursor::new(fsm))
+                            }
+                            _ => {
+                                handle_client(req, &mut cursor.as_mut().expect("didn't call init!"))
+                            }
+                        }
+                    }
+                    stream
+                        .shutdown(std::net::Shutdown::Both)
+                        .expect("stream shutdown");
+                }));
             }
             Err(err) => {
                 /* connection failed */
@@ -36,5 +60,8 @@ fn main() -> std::io::Result<()> {
             }
         }
     }
+    handles.into_iter().for_each(|h| {
+        h.join().unwrap();
+    });
     Ok(())
 }
