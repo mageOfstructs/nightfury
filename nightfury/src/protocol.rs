d117b6c67540540c4d4252092372c4c5690cd059
mageOfStructs <jason.puschnig@gmx.at>
2025-06-23 11:34:52 +0200

    add write_with_null trait

--- nightfury/src/protocol.rs
+++ nightfury/src/protocol.rs
@@ -1,6 +1,8 @@
+use std::io::Write;
+
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub enum Request {
     Init(String),
     GetCapabilities,
@@ -9,9 +11,27 @@ pub enum Request {
     Reset,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub enum Response {
     Capabilities(Vec<String>),
     Expanded(String),
     Ok,
+}
+
+pub trait WriteNullDelimitedExt {
+    fn write_with_null(&mut self, data: &[u8]) -> std::io::Result<()>;
+    fn write_with_null_flush(&mut self, data: &[u8]) -> std::io::Result<()>;
+}
+
+impl<S: Write> WriteNullDelimitedExt for S {
+    fn write_with_null(&mut self, data: &[u8]) -> std::io::Result<()> {
+        self.write(data)?;
+        self.write(&[0])?;
+        Ok(())
+    }
+    fn write_with_null_flush(&mut self, data: &[u8]) -> std::io::Result<()> {
+        self.write_with_null(data)?;
+        self.flush()?;
+        Ok(())
+    }
 }
