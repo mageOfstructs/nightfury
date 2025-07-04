d7c1499c64cf82441867a63fd9543660d55bd5d0
mageOfStructs <jason.puschnig@gmx.at>
2025-07-04 23:27:43 +0200

    start working on the new protocol

--- nightfury/src/protocol.rs
+++ nightfury/src/protocol.rs
@@ -2,14 +2,14 @@ use std::io::{BufRead, ErrorKind, Read, Write};
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
-pub enum Request {
-    Init(String),
-    GetCapabilities,
-    Advance(char),
-    AdvanceStr(String),
-    Reset,
-}
+// #[derive(Serialize, Deserialize, Debug)]
+// pub enum Request {
+//     Init(String),
+//     GetCapabilities,
+//     Advance(char),
+//     AdvanceStr(String),
+//     Reset,
+// }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Response {
@@ -49,5 +49,68 @@ impl<S: Write> WriteNullDelimitedExt for S {
         self.write_with_null(data)?;
         self.flush()?;
         Ok(())
+    }
+}
+
+#[repr(u8)]
+enum Request {
+    GetCapabilities,
+    InstallLanguage(String, Option<String>),
+    Initialize(String),
+    Revert,
+    Reset,
+    SetCursor(u16),
+}
+
+#[derive(Debug)]
+enum RequestParseError {
+    Empty,
+    InvalidControlCode,
+    InvalidEncoding,
+}
+
+const MAX_REQ_CONTROL_CODE: u8 = 7u8;
+
+trait ReadRequest {
+    fn read_request(&mut self) -> Request;
+}
+
+impl<R: BufRead> ReadRequest for R {
+    fn read_request(&mut self) -> Request {
+        let mut buf = Vec::with_capacity(2);
+        self.read_until(0, &mut buf);
+        Request::try_from(buf).unwrap()
+    }
+}
+
+impl TryFrom<Vec<u8>> for Request {
+    type Error = RequestParseError;
+    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+        if value.is_empty() {
+            return Err(RequestParseError::Empty);
+        }
+        match value[0] {
+            0x01 => Ok(Request::GetCapabilities),
+            0x02 => todo!(),
+            0x03 => {
+                if value.len() < 3 {
+                    return Err(RequestParseError::Empty);
+                }
+                match str::from_utf8(&value[1..value.len() - 1]) {
+                    Ok(str) => Ok(Request::Initialize(str.to_string())),
+                    Err(_) => Err(RequestParseError::InvalidEncoding),
+                }
+            }
+            0x04 => Ok(Request::Revert),
+            0x05 => Ok(Request::Reset),
+            0x06 => {
+                if value.len() < 3 {
+                    return Err(RequestParseError::Empty);
+                }
+                let cursor_handle = (value[1] as u16) << 8 | value[2] as u16;
+                Ok(Request::SetCursor(cursor_handle))
+            }
+            _ => Err(RequestParseError::InvalidControlCode),
+        }
     }
 }
