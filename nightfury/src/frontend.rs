636688f2f689a54fb734581c945a6a03ca0e4425
mageOfStructs <jason.puschnig@gmx.at>
2025-07-14 09:10:12 +0200

    rewrite handle_node to move Node, avoiding some clones

--- nightfury/src/frontend.rs
+++ nightfury/src/frontend.rs
@@ -21,21 +21,21 @@ enum TerminalState {
 }
 
 fn handle_node(
-    grammar: &Grammar,
-    cur_node: &Node,
+    grammar: &mut Grammar,
+    cur_node: Node,
     cur_root: &FSMRc<FSMLock<FSMNode>>,
     terminals: &mut HashMap<String, (FSMRc<FSMLock<FSMNode>>, TerminalState)>,
 ) -> FSMRc<FSMLock<FSMNode>> {
     debug_println!("handle_node got {:?}", cur_node);
-    let ret = match &cur_node {
+    let ret = match cur_node {
         Node::String(str) => {
             FSMNode::new_keyword_with_parent(str.to_string(), FSMRc::clone(cur_root))
         }
-        Node::RegexString(r) => FSMNode::new_userdef(Regex::new(r).unwrap(), cur_root),
+        Node::RegexString(r) => FSMNode::new_userdef(Regex::new(&r).unwrap(), cur_root),
         Node::Terminal(name) => {
-            if terminals.contains_key(name) {
+            if terminals.contains_key(&name) {
                 debug_println!("Found {name} in cache!");
-                let term = terminals.get(name).unwrap();
+                let term = terminals.get(&name).unwrap();
                 let term_clone = match term.1 {
                     TerminalState::Stub => term.0.clone(),
                     TerminalState::Created => term.0.borrow().deep_clone(),
@@ -55,14 +55,11 @@ fn handle_node(
                     name.to_string(),
                     (FSMRc::clone(&term_root), TerminalState::Stub),
                 );
-                handle_node(grammar, &terminal.rhs, &term_root, terminals);
-                terminals.insert(
-                    name.to_string(),
-                    (FSMRc::clone(&term_root), TerminalState::Created),
-                );
+                handle_node(grammar, terminal.rhs.clone(), &term_root, terminals);
                 debug_println!("Finish terminal");
                 debug_println!("young {}:", name);
                 term_root.borrow().dbg();
+                terminals.insert(name, (FSMRc::clone(&term_root), TerminalState::Created));
                 let ret = term_root.borrow().deep_clone();
                 FSMNode::add_child_cycle_safe(cur_root, &ret);
                 ret
@@ -70,9 +67,9 @@ fn handle_node(
         }
         Node::Multiple(nodes) => {
             let mut cur_treenode = cur_root.clone();
-            nodes.iter().for_each(|node| {
+            nodes.into_iter().for_each(|node| {
                 debug_println!("Multiple at {node:?}");
-                let tree_bit = handle_node(grammar, &node, &cur_treenode, terminals);
+                let tree_bit = handle_node(grammar, node, &cur_treenode, terminals);
                 debug_println!("Multiple got back:");
                 tree_bit.borrow().dbg();
                 // NOTE: this will only work as long as the other node handlers nicely merge their
@@ -83,22 +80,22 @@ fn handle_node(
             cur_treenode
         }
         Node::RegexExt(node, RegexExtKind::Optional) | Node::Optional(node) => {
-            let tree_bit = handle_node(grammar, &node, cur_root, terminals);
+            let tree_bit = handle_node(grammar, *node, cur_root, terminals);
             let dummy = FSMNode::new_null(None);
             FSMNode::add_child_to_all_leaves(&tree_bit, &dummy);
             FSMNode::add_child_cycle_safe(&cur_root, &dummy);
             tree_bit
         }
         Node::Symbol(n1, SymbolKind::Concatenation, n2) => {
-            let t1 = handle_node(grammar, &n1.to_owned(), &cur_root, terminals);
-            let _ = handle_node(grammar, &n2.to_owned(), &t1, terminals);
+            let t1 = handle_node(grammar, *n1, &cur_root, terminals);
+            let _ = handle_node(grammar, *n2, &t1, terminals);
             t1
         }
         Node::Symbol(n1, SymbolKind::Alternation, n2) => {
             let root = FSMNode::new_null(Some(cur_root)); // need this here otherwise minify will
             // crash?
-            let _ = handle_node(grammar, &n1.to_owned(), &root, terminals);
-            let _ = handle_node(grammar, &n2.to_owned(), &root, terminals);
+            let _ = handle_node(grammar, *n1, &root, terminals);
+            let _ = handle_node(grammar, *n2, &root, terminals);
             let child = FSMNode::new_null(None);
             debug_println!("Alternation dummy child: {}", child.borrow().short_id());
             FSMNode::add_child_to_all_leaves(&root, &child);
@@ -106,12 +103,12 @@ fn handle_node(
             root.borrow().dbg();
             root
         }
-        Node::Group(node) => handle_node(grammar, node, cur_root, terminals),
+        Node::Group(node) => handle_node(grammar, *node, cur_root, terminals),
         Node::Repeat(node) => {
             // need to guarantee this is a null so search_rec won't prematurely stop, e.g. when
             // cur_root is a Keyword
             let dummy_parent = FSMNode::new_null(Some(&cur_root));
-            let subroot = handle_node(grammar, &node, &dummy_parent, terminals);
+            let subroot = handle_node(grammar, *node, &dummy_parent, terminals);
 
             let dummy = FSMNode::new_null(None);
             debug_println!("Repeat dummy child: {}", dummy.borrow().short_id());
@@ -138,13 +135,13 @@ fn find_terminal<'a>(grammer: &'a Grammar, name: &'a str) -> Option<&'a Expressi
 /// Returns an error if the ebnf crate deems the input to be invalid
 pub fn create_graph_from_ebnf(ebnf: &str) -> Result<FSMRc<FSMLock<FSMNode>>, String> {
     match ebnf::get_grammar(ebnf) {
-        Ok(grammar) => {
+        Ok(mut grammar) => {
             let root = FSMNode::new_null(None);
-            let root_node = grammar.expressions.get(0).ok_or("Empty BNF!")?;
-            let mut terminals = HashMap::new();
+            let root_node = grammar.expressions.get(0).ok_or("Empty BNF!")?.to_owned();
+            let mut terminals = HashMap::with_capacity(grammar.expressions.len());
             handle_node(
-                &grammar,
-                &Node::Terminal(root_node.lhs.to_owned()),
+                &mut grammar,
+                Node::Terminal(root_node.lhs.to_owned()),
                 &root,
                 &mut terminals,
             );
@@ -162,5 +159,20 @@ pub fn create_graph_from_ebnf(ebnf: &str) -> Result<FSMRc<FSMLock<FSMNode>>, Str
             Ok(root)
         }
         Err(err) => Err(err.to_string()),
+    }
+}
+
+extern crate test;
+
+#[cfg(test)]
+mod tests {
+    use std::fs::read_to_string;
+
+    use super::*;
+    use test::Bencher;
+    #[bench]
+    fn benchmark(b: &mut Bencher) {
+        let ebnf = read_to_string("../js.ebnf").unwrap();
+        b.iter(|| create_graph_from_ebnf(&ebnf));
     }
 }
